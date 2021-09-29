6e45732434db5e63bb45860b08f57037713d3c82
wxydejoy <1798923421@qq.com>
2021-09-29 11:00:51 +0800

    更新界面

--- serve/ServerDemo/resource.h
+++ serve/ServerDemo/resource.h
@@ -20,13 +20,19 @@
 #define IDC_STATIC_Text1                1006
 #define IDC_STATIC_Text2                1007
 #define IDC_EDIT_PassWord               1007
+#define IDC_BUTTON_SendData4            1007
 #define IDC_STATIC_Text3                1008
 #define IDC_PIC                         1008
 #define IDC_BUTTON_Reset                1008
-#define IDC_MFCLINK1                    1010
-#define IDC_EDIT2                       1012
-#define IDC_EDIT4                       1015
-#define IDC_Time                        1015
+#define IDC_BUTTON_SendData5            1008
+#define IDC_BUTTON_SendData6            1009
+#define IDC_Time2                       1016
+#define IDC_Time3                       1017
+#define IDC_COMBO1                      1018
+#define IDC_COMBO2                      1019
+#define IDC_CHECK1                      1020
+#define IDC_CHECK2                      1021
+#define IDC_CHECK3                      1022
 
 // Next default values for new objects
 // 
@@ -34,7 +40,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_CONTROL_VALUE         1021
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
